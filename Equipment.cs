2da739a8a57b95e585e69da62dd2859795a6443f
Elucid99 <seanm@seycove.ca>
2017-11-12 00:56:36 -0400

    Add files via upload

---
+++ Equipment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item {
+
+    public EquipmentSlot equipSlot;
+
+    public int armorModifier;
+    public int damageModifier;
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);
+        RemoveFromInventory();
+        Debug.Log("1");
+    }
+}
+
+public enum EquipmentSlot { Head, Chest, Legs, Boots, Tool, Backpack}
---
+++ Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject {
+
+    new public string name = "New Item";
+    public string description = "New Description";
+    public Sprite icon = null;
+    public int id = 0;
+    public int capacity = 0;
+    public int maxStackSize = 1;
+    public int curStackSize = 0;
+
+    public virtual void Use()
+    {
+        Debug.Log("Using: " + name);
+        Debug.Log("2");
+    }
+
+    public void RemoveFromInventory()
+    {
+        Inventory.instance.Remove(this);
+        Debug.Log("4");
+    }
+}
